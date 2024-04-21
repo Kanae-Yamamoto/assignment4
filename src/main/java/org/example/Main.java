b4abafdcd79707f324f816a495bc8ac504bb3fbe
Kanae-Yamamoto <chankana0504@yahoo.co.jp>
2024-04-21 17:11:28 +0900

    ラムダ式をメソッド参照に修正

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<String> names = List.of("mike","tama","tora","kuro");
-        List<String> result = names.stream().filter(name -> name.startsWith("t")).toList();
-        System.out.println(result);
+        names.stream().filter(name -> name.startsWith("t")).forEach(System.out::println);
     }
 }
